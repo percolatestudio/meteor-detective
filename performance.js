a95cef75e9d1bf357719eafceab79521a224fe8b
Zoltan Olah <zol@percolatestudio.com>
2014-07-03 17:37:27 -0700

    Added count to size, confusing I know

--- performance.js
+++ performance.js
@@ -1,13 +1,17 @@
 var LocalCollectionDriver = Package['mongo-livedata'].LocalCollectionDriver;
 
 Performance = {
+  _totalDataSize: 0,
+
   collectionSize: function(collection) {
     var size = 0;
     collection.find().forEach(function(doc) {
       // XXX: unicode etc all -- length is not the right metric
-      size += EJSON.stringify(doc).length;
+      // ZOL: Use browser's native JSON for speed and dont worry about
+      // binary data/extended attributes for now.
+      size += JSON.stringify(doc).length;
     });
-    return size;
+    return {size: size, count: collection.find().count()};
   },
   
   collectionSizeByName: function() {
@@ -21,8 +25,24 @@ Performance = {
   // only works on client
   allCollectionsSize: function() {
     return _.reduce(Performance.collectionSizeByName(), function(size, total) {
-      return size + total;
+      return size.size + total;
     });
+  },
+  
+  logAllCollections: function() {
+    var sizes = this.collectionSizeByName();
+    var totalSize = 0, totalCount = 0;
+
+    var str = '';
+    _.each(sizes, function(size, name) {
+      str += name + '-> size:' + size.size + ' count: ' + size.count + '\n';
+      totalSize += size.size;
+      totalCount += size.count;
+    });
+    
+    str += '=======\nTOTAL-> size:' + totalSize + ' count:' + totalCount;
+    
+    console.log(str);
   },
   
   startMeasuring: function() {
@@ -32,22 +52,29 @@ Performance = {
     
     Meteor.connection._stream.on('message', function(data) {
       self._dataSize += data.length;
+      self._totalDataSize += data.length;
     });
   },
   
   takeMeasurement: function() {
     console.log('Took', (new Date - this._startTime) / 1000, 'seconds');
     console.log('Received', this._dataSize, 'bytes of data');
+  },
+  
+  takeInBetweenMeasurement: function() {
+    console.log('Received', this._dataSize, 'bytes between routes');
   }
 }
 
 Performance.startMeasuring();
 
 Router.onRun(function() {
-  console.log("In between Routes:")
-  console.log("__________________")
-  Performance.takeMeasurement();
-  console.log('');
+  // console.log("In between Routes:")
+  // console.log("__________________")
+  // Performance.takeMeasurement();
+  // console.log('');
+  
+  Performance.takeInBetweenMeasurement();
   
   Performance.startMeasuring();
 });
