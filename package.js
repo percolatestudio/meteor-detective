8e1e1f0608bd257a33a35bb3f8b378543608792f
Zoltan Olah <zol@percolatestudio.com>
2014-11-13 16:51:51 -0800

    Version 0.2.0 supporting IR 1.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Measure performance of publications and subscriptions",
-  version: "0.1.0",
+  version: "0.2.0",
   name: "percolate:detective",
   git: "https://github.com/percolatestudio/meteor-detective.git"
 });
@@ -10,7 +10,7 @@ Package.on_use(function (api, where) {
 
   api.add_files('detective.js', 'client');
   
-  api.use(['mongo-livedata', 'iron:router']);
+  api.use(['mongo-livedata', 'iron:router@1.0.1']);
   
   api.export('Detective')
 });
